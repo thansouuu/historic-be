ea468539c9049c2ed34b6ea9c807b71708d60689
buhkhkt <khoahhockithuat2023win@gmail.com>
2024-07-14 01:21:49 +0700

    updatet

--- src/configs/database/index.js
+++ src/configs/database/index.js
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 export const db = () =>
     mongoose.connect(process.env.MONGODB_URL, {
         autoCreate: true,
-        dbName: 'review-foods',
+        dbName: 'explore-history',
     });
